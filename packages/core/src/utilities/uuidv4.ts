128b5d35db73130da636ce6c010371dae6d5d6fb
Mohammad Salah <m.salah@alifbee.com>
2024-08-13 22:36:41 +0300

    Added fallback to uuidv4 function using 'uuid' package when crypto.randomUUID is not available

--- packages/core/src/utilities/uuidv4.ts
+++ packages/core/src/utilities/uuidv4.ts
@@ -1,3 +1,5 @@
+import { v4 as uuidv4Fallback } from 'uuid';
+
 /**
  * Generates a unique id that has limited chance of collision
  *
@@ -6,7 +8,11 @@
  */
 export default function uuidv4(): string {
   // @ts-ignore
-  return crypto.randomUUID?.() || ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-  );
+  if (typeof crypto !== 'undefined' && crypto.randomUUID) {
+    return crypto.randomUUID?.() || ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  } else {
+    return uuidv4Fallback();
+  }
 }
